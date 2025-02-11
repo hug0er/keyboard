74248a38e701f4eed1dbdd4663a841bce51cbcf6
Hugo Betancourt <hugo.e.betancourt@gmail.com>
2025-02-10 23:20:34 -0500

    Update question mark import

--- keyboards/crkbd/rev1/keymaps/hug0er/keymap.c
+++ keyboards/crkbd/rev1/keymaps/hug0er/keymap.c
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_TRNS,  KC_EXLM,  KC_AT,  KC_HASH,  KC_DLR,  KC_PERC,                    KC_CIRC, KC_AMPR, KC_PIPE, KC_LPRN, KC_RPRN, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_PLUS, KC_UNDS, KC_LT,                         KC_GT, KC_MINUS, KC_GRV, KC_LBRC, KC_RBRC, KC_ASTR,
+      KC_TRNS, KC_TRNS, KC_QUES, KC_PLUS, KC_UNDS, KC_LT,                         KC_GT, KC_MINUS, KC_GRV, KC_LBRC, KC_RBRC, KC_ASTR,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_TRNS, KC_QUES, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_PPLS, KC_EQL, KC_BSLS, KC_LCBR, KC_RCBR, KC_TILD,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
